4318ae24c9e98eea150f09cf402a3d924fad607d
Li meng <li487@iupui.edu>
2020-01-19 14:44:33 +0800

    init

--- run.R
+++ run.R
@@ -88,7 +88,7 @@ t_igraph_list<-get_members(t_igraph_list,t_alpha);
 t_igraph_list<-cal_mlp(t_igraph_list,"./result/best_order.tsv",t_alpha,read_count_threshold);
 
 ##draw intron splicing order graph
-t_igraph_list<-draw_3d(t_igraph_list,"./result/html/",t_alpha,TRUE);
+t_igraph_list<-draw_3d(t_igraph_list, paste0(getwd(),"./result/html/"),t _alpha,TRUE);
 
 ##output intron splicing order adjacent matrix
 for( i in 1:length(t_igraph_list) ){
