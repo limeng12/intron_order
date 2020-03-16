0e8ace9ffb912bfd9230e5943664488881ebad7f
Li meng <li487@iupui.edu>
2020-03-16 08:09:06 +0800

    first commit

--- run.R
+++ run.R
@@ -3,7 +3,7 @@ this.dir<-dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(this.dir);
 
 
-files_all<-list.files("data/",full.names =TRUE,pattern = "*tsv");
+files_all_iso_pairs<-list.files("data/",full.names =TRUE,pattern = "*tsv");
 
 gene_trans_id_tbl<-"./anno/hg19_ensembl_gene_id_trans_id_map.tsv";
 
@@ -16,7 +16,7 @@ read_count_threshold<-0;
 source("code/necessary_code.R");
 #################################################################################################################
 
-t_igraph_list<-get_mlo_pipe(bed_anno,files_all,most_likeli_order_output_path,
+t_igraph_list<-get_mlo_pipe(bed_anno,files_all_iso_pairs,most_likeli_order_output_path,
                             gene_trans_id_tbl,read_count_threshold);
 
 #save( t_igraph_list, file="result/t_igraph_list.Rd",version = 2);
